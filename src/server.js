ff1779b3a585fdd52ba56d2fdb5c9193498499f7
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-11-28 00:54:43 -0300

    creating an Universally unique identifier (UUID)

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@
 // HTTP Status Code
 
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
 
@@ -37,7 +38,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body;
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     };
